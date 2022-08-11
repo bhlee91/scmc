de1fd0e2ef6cf9eee4d6508d5ecb75532303ce8d
MoonSeHwan <msh7805@gmail.com>
2022-08-11 10:04:20 +0900

    gno -> zno 변경

--- ScmcApp/android/app/src/main/java/com/gno/scmcapp/MainApplication.java
+++ ScmcApp/android/app/src/main/java/com/gno/scmcapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.gno.scmcapp;
+package com.zno.scmcapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -10,7 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.gno.scmcapp.TMapPackage;
+import com.zno.scmcapp.TMapPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.gno.scmcapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.zno.scmcapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
