be929cdb49d74ef43c4cc600fdcd17e3e2dd8fd9
ZnOSoft_D01\ZnOSoftD01 <msh7805@gmail.com>
2022-08-26 16:10:34 +0900

    권한 해제

--- scmc-api/src/main/java/com/scmc/api/common/config/SecurityConfig.java
+++ scmc-api/src/main/java/com/scmc/api/common/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 					.and()
 				.authorizeHttpRequests()
 				.antMatchers(
-						"/",
+						"/**",
 						"/auth/**",
 						"/swagger/**", 
 		        		"/swagger-ui.html", 
